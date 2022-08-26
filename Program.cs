4f0b0cd055cf2387e710d976f883db47d4764b7f
Derek Griffin <griffinderek6@gmail.com>
2022-08-26 17:28:47 -0600

    finished

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace StaticExersice1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var celsius = TempConverter.FahrenheitToCelsius(68);
+            var fahrenheit = TempConverter.CelsiusToFahrenheit(20);
+
+            Console.WriteLine("After conversion: ");
+            Console.WriteLine($"fahrenheit {fahrenheit}");
+            Console.WriteLine($"celsius {celsius}");
+        }
+    }
+}
+
